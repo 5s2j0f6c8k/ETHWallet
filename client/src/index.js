07a5e93942cb06120929924aa74463b4232c373b
renger <qgass@163.com>
2018-01-09 13:43:02 +0800

    添加样式

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,8 @@
 /*
  * @Author: qugang 
  * @Date: 2018-01-08 00:12:07 
- * @Last Modified by:   qugang 
- * @Last Modified time: 2018-01-08 00:12:07 
+ * @Last Modified by: qugang
+ * @Last Modified time: 2018-01-09 13:42:14
  */
 
 
@@ -11,7 +11,7 @@ import React, { Component } from 'react'
 import ReactDOM from 'react-dom';
 import '../node_modules/onsenui/css/onsenui.css';
 import '../node_modules/onsenui/css/onsen-css-components.css';
-
+import "./css/style.css"
 import ons from 'onsenui';
 
 import {
